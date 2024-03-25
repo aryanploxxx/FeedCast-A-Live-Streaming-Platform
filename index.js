51f014035f5161e32db638fdb379fbb77e556396
aryanploxxx <aryangupta8291@gmail.com>
2024-03-26 02:35:31 +0530

    Basic Server Setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+// To use Web Sockets in the project
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.static(path.resolve('./public')))
+// This will allow us to keep all our HTML, CSS and JS files int the Public Folders
+
+server.listen(3000, () => console.log("Server is running on port 3000!"))
