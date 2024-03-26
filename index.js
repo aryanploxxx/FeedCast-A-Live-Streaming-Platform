c22b178ed6ca7534b0578e7dc1abd6a1e8fc2c52
aryanploxxx <aryangupta8291@gmail.com>
2024-03-26 12:00:30 +0530

    Recieve binary stream on backend

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const SocketIO = require('socket.io').Server;
 const app = express()
 const server = http.createServer(app)
 
+const io = new SocketIO(server)
+// We passed our server instance in it
+// Now we can use this 'io' to handle our socket events
+
 app.use(express.static(path.resolve('./public')))
 // This will allow us to keep all our HTML, CSS and JS files int the Public Folders
+
+io.on('connection', socket => {
+    console.log("Socekt Connected: ", socket.id)
+    socket.on('binarystream', stream => {
+        console.log('Binary Stream Incoming!')
+    })
+    // We have recieved this stream on backend, now we just have to throw it on ffmpeg
+})
 
 server.listen(3000, () => console.log("Server is running on port 3000!"))
--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ const startButton = document.getElementById("start-btn")
 
 const state = {media: null}
 // Get access to media object globally
+const socket = io()
 
 startButton.addEventListener('click', (e)=> {
     const mediaRecorder = new MediaRecorder(state.media, {
@@ -20,10 +21,22 @@ startButton.addEventListener('click', (e)=> {
         // After clicking on start button, check the logs to see if binary data is being recorded
         // If successfull, pass this binary stream to the backed servers using sockets
         socket.emit('binarystream', ev.data)
+        /*
+            now we need to send the stream inside ev.data to backend 
+            binarystream just acts like a variable name, could be anything
+        */
     }
 
     mediaRecorder.start(25)
-    // Start cpaturing at frame-rate 25
+    /*
+        Begins recording media; this method can optionally be passed a timeslice argument 
+        with a value in milliseconds. If this is specified, the media will be captured in 
+        separate chunks of that duration, rather than the default behavior of recording the 
+        media in a single large chunk.
+        -> https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder
+    */
+
+
 })
 
 window.addEventListener('load', async (e)=> {
